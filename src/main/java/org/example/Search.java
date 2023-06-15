c1c273f2a12d3fd94d8422efa40b10d21f637cce
Alena <gertmanea@mail.ru>
2023-06-15 19:23:36 +0300

    "лекция 3 алгоритмы списки"

--- src/main/java/org/example/Search.java
+++ src/main/java/org/example/Search.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class Search {
@@ -11,15 +12,64 @@ public class Search {
         Integer length = scanner.nextInt();
 
         int[] array = interArray(length);
-//        int[] array = new int[]{2, 57, 52, 1, 52, 88, 94, 41, 93};
-        Integer x = array.length / 2 - 1;
-        Integer end = array.length -1;
-        heap(array, x, end);
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+
+//        Integer x = array.length / 2 - 1;
+//        Integer end = array.length -1;
+//        Date startSort = new Date();
+//        heap(array, x, end);
+//        Date endSort = new Date();
+//        long time1 = endSort.getTime() - startSort.getTime();
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.print(array[i] + " ");
+//        }
+
+        // второй вариант из интернета
+        // построение кучи
+        Date startSort2 = new Date();
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            hearyfy(array, array.length, i);
         }
+        // один за другим извлекаем элементы из кучи
+        for (int i = array.length - 1; i >= 0; i--) {
+            // перемещаем текущий корень в конец
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // вызваем процедуру hearyfy на уменьшенной куче
+            hearyfy(array, i, 0);
+        }
+        Date endSort2 = new Date();
+        long time2 = endSort2.getTime() - startSort2.getTime();
+        System.out.println();
+        for (int o = 0; o < array.length; o++) {
+            System.out.print(array[o] + " ");
+        }
+        System.out.println();
+//        System.out.printf("time1 = %d, time2 = %d", time1, time2);
 
     }
+
+    private static void hearyfy(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex;
+        int leftChild = rootIndex * 2 + 1;
+        int rightChild = rootIndex * 2 + 2;
+
+        if (leftChild < heapSize && array[leftChild] > array[rootIndex]){
+            largest = leftChild;
+        }
+        if (rightChild < heapSize && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+        if (largest != rootIndex){ //если самый большой элемент не корень
+            int temp = array[largest];
+            array[largest] = array[rootIndex];
+            array[rootIndex] = temp;
+
+            hearyfy(array, heapSize, largest);
+        }
+    }
+
     public static int[]interArray (int length) {
         int[] array = new int[length];
         for (int i = 0; i < length; i++) {
